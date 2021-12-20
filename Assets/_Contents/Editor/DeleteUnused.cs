befc9e941bd66052f40aecfa864e1b7e5035b2f7
Tomohiko Toyama <nanaki.program@gmail.com>
2021-12-20 23:34:03 +0900

    複数選択可能なよう修正

--- Assets/_Contents/Editor/DeleteUnused.cs
+++ Assets/_Contents/Editor/DeleteUnused.cs
@@ -8,34 +8,38 @@ public class DeleteUnused : EditorWindow
     [MenuItem("GameObject/Tools/Timeline/Unusedを消去",false,1)]
     static void Delete()
     {
-        List<GameObject> objects = new List<GameObject>();
-        var pd =  Selection.activeGameObject.GetComponent<PlayableDirector>();
-        var asset = pd.playableAsset;
-        var path = AssetDatabase.GetAssetPath(asset);
-        // シリアライズ化
-        var so = new SerializedObject(pd);
-        so.Update();
-
-        // m_SceneBindingsの変数を取得
-        var prop = so.FindProperty("m_SceneBindings");
-        
-        for (int i = 0; i < prop.arraySize; i++)
+        GameObject[] gameObjects = Selection.gameObjects;
+        foreach (var obj in gameObjects)
         {
-            // m_SceneBindingsの配列のi番目のkeyのプロパティを取得
-            var key = prop.GetArrayElementAtIndex(i).FindPropertyRelative("key");
-            // keyの参照しているオブジェクトのインスタンスIDを取得
-            var tmp = key.objectReferenceInstanceIDValue;
-            // インスタンスIDを元にアセットのパスを取得する
-            var propPath = AssetDatabase.GetAssetPath(tmp);
-            Debug.Log(propPath);
-            
-            if (path != propPath)
+            var pd =  obj.GetComponent<PlayableDirector>();
+            var asset = pd.playableAsset;
+            var path = AssetDatabase.GetAssetPath(asset);
+            // シリアライズ化
+            var so = new SerializedObject(pd);
+            so.Update();
+
+            // m_SceneBindingsの変数を取得
+            var prop = so.FindProperty("m_SceneBindings");
+        
+            for (int i = 0; i < prop.arraySize; i++)
             {
-                prop.DeleteArrayElementAtIndex(i);
-                i--;
+                // m_SceneBindingsの配列のi番目のkeyのプロパティを取得
+                var key = prop.GetArrayElementAtIndex(i).FindPropertyRelative("key");
+                // keyの参照しているオブジェクトのインスタンスIDを取得
+                var tmp = key.objectReferenceInstanceIDValue;
+                // インスタンスIDを元にアセットのパスを取得する
+                var propPath = AssetDatabase.GetAssetPath(tmp);
+                Debug.Log(propPath);
+            
+                if (path != propPath)
+                {
+                    prop.DeleteArrayElementAtIndex(i);
+                    i--;
+                }
             }
+            so.ApplyModifiedProperties();
         }
-        so.ApplyModifiedProperties();
+        
         Debug.Log("完了");
     }
 }
